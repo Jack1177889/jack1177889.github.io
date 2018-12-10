27cbaf11cbc5dc33e8a2215cb55780c8f8d116d3
josh 1177889 <jack1177889>
2018-12-10 08:46:25 -0600

    s

--- f2/js/index.js
+++ f2/js/index.js
@@ -1,5 +1,9 @@
 function annoy(){
+
 var fname=prompt("tell me your name");
+
+document.getElementById("annoy").innerHTML  = "I am annoed";
+
 alert(fname +" are we there yet?");
 alert(fname +" are we there yet?");
 alert(fname +" are we there yet?");
