2b6b17ab593d2f2504e0c148a5ff3f278a45ff6c
josh 1177889 <jack1177889>
2018-12-10 08:49:40 -0600

    s

--- f2/js/index.js
+++ f2/js/index.js
@@ -4,7 +4,8 @@ var fname=prompt("tell me your name");
 
 document.getElementById("annoy").innerHTML  = "I am annoed";
 
+while(true){
 alert(fname +" are we there yet?");
-alert(fname +" are we there yet?");
-alert(fname +" are we there yet?");
+}
+
 }
