97e2ac226b8c84f6d4956a0e75e36dc4fdf5f4bf
josh 1177889 <jack1177889>
2018-12-12 08:35:50 -0600

    s

--- f2/js/index.js
+++ f2/js/index.js
@@ -13,7 +13,22 @@ function mark_present()
 {
     alert("yeet");
     var cadet_name = $('#cadet_name').val();
-    alert("asdfasdf");
     alert(cadet_name+ " is here!");
 }
 
+function mark_present()
+{
+    var cadet_name = $('#cadet_name').val();
+    alert(cadet_name+"is here!");
+    $('#present_cadets').append("<ul id = 'cadets_list'></ul>");
+    $('#caders_list').append("<l1>")+cadet_name+"</l1>");
+}
+
+$(document).ready(function(){
+    var flag = false;
+    setInterval(function() {
+        flag = !flag;
+        $("body").css("background", flag ? "red" : "green");
+        $("body").css("background", flag ? "green" : "red");
+    }
+}
