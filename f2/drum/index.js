c8402d13c49c5a02ca5e5b1939d08531aea98371
josh 1177889 <jack1177889>
2018-12-18 08:51:47 -0600

    s

--- f2/drum/index.js
+++ f2/drum/index.js
@@ -11,6 +11,15 @@ function removeTransition(e) {
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
+
+    function playSound(e) {
+      const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+      const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+      if (!audio) return;
+      key.classList.add('playing');
+      audio.currentTime = 0;
+      audio.play();
+
   }
   const keys = Array.from(document.querySelectorAll('.key'));
   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
